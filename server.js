be679efe9b2a3b069202580f16935efe868a20c0
GarudaMartTech <garudamarttech@gmail.com>
2023-12-01 12:21:34 +0530

    update

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
 
 const PORT = 4000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello world")
-})
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log(`db connected successfull ${res.connection.host}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-app.listen(PORT,()=>{
-    console.log(`server is running at PORT : ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running at PORT : ${PORT}`);
+});
